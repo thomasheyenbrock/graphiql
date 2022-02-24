070fe581ec8bc00e6b20d071b5e2d196fbbceefc
Rikki Schulte <rikki.schulte@gmail.com>
2022-02-24 20:42:36 +0100

    use graphql@16.3.0, matrix for the range

--- packages/graphql-language-service-interface/src/__tests__/getAutocompleteSuggestions-test.ts
+++ packages/graphql-language-service-interface/src/__tests__/getAutocompleteSuggestions-test.ts
@@ -121,6 +121,19 @@ describe('getAutocompleteSuggestions', () => {
       });
   }
   describe('with Operation types', () => {
+    const expectedDirectiveSuggestions = [
+      { label: 'include' },
+      { label: 'skip' },
+    ];
+
+    console.log({ graphQLVersion });
+
+    // TODO: remove this once defer and stream are merged to `graphql`
+    if (graphQLVersion.includes('defer')) {
+      console.log('expect stream');
+      expectedDirectiveSuggestions.push({ label: 'stream' });
+    }
+    expectedDirectiveSuggestions.push({ label: 'test' });
     it('provides correct sortText response', () => {
       const result = getAutocompleteSuggestions(
         schema,
@@ -431,16 +444,6 @@ describe('getAutocompleteSuggestions', () => {
         { label: 'CharacterDetails2', detail: 'Human' },
       ]);
     });
-
-    const expectedDirectiveSuggestions = [
-      { label: 'include' },
-      { label: 'skip' },
-    ];
-    // TODO: remove this once defer and stream are merged to `graphql`
-    if (graphQLVersion.includes('defer')) {
-      expectedDirectiveSuggestions.push({ label: 'stream' });
-    }
-    expectedDirectiveSuggestions.push({ label: 'test' });
 
     it('provides correct directive suggestions', () => {
       expect(testSuggestions('{ test @ }', new Position(0, 8))).toEqual(
